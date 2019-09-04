248defcac1a4720b41b1bd29593e4e2af7a499dc
Matt Garnett <14004106+lightclient@users.noreply.github.com>
2019-09-04 13:34:01 -0400

    simplify logic, add comments, and additional test case

--- serialized_multiproof/verify.py
+++ serialized_multiproof/verify.py
@@ -6,30 +6,27 @@ def verify(offsets, proof, offset=0):
     if len(offsets) == 0:
         return proof[offset]
 
-    if offsets[0] == 1:
-        left = proof[offset]
-        right = proof[offset + 1]
+    left = proof[offset]
+    right = proof[offset + 1]
 
-        if len(offsets) != 1:
-            right = verify(
-                offsets[offsets[0]:],
-                proof,
-                offsets[0] + offset,
-            )
-
-        return hash(left, right)
-
-    left = verify(
-        offsets[1:(offsets[0])],
-        proof,
-        offset,
-    )
-
-    right = verify(
-        offsets[offsets[0]:],
-        proof,
-        offsets[0] + offset,
-    )
+    # If the first offset is not one, then we have not reached an end node
+    # so continue to recurse.
+    if offsets[0] != 1:
+        left = verify(
+            offsets[1:(offsets[0])],
+            proof,
+            offset,
+        )
+    
+    # If there is only 1 offset, then we can be sure that we've reached the
+    # bottom of the tree and the right node can be inferred as left_offset + 1,
+    # Otherwise, recurse on the right side.
+    if len(offsets) != 1:
+        right = verify(
+            offsets[offsets[0]:],
+            proof,
+            offsets[0] + offset,
+        )
 
     return hash(left, right)
 
@@ -52,9 +49,40 @@ def zh(depth):
 
 class TestOffsetBuilder(unittest.TestCase): 
     def test_simple(self):
+        # indexes = [4, 10, 11, 3]
         offsets = [3, 1, 1]
         proof = [zh(1), zh(0), zh(0), zh(2)]
+
         self.assertEqual(verify(offsets, proof), zh(3))
+
+    def test_full(self):
+        # indexes = [8, 9, 10, 11, 12, 13, 14, 15]
+        offsets = [4, 2, 1, 1, 2, 1, 1]
+        proof = [zh(0)] * 8
+
+        self.assertEqual(verify(offsets, proof), zh(3))
+
+    def test_random(self):
+        # indexes = [2, 12, 26, 27, 7]
+        offsets = [1, 3, 1, 1]
+        proof = [
+            hash("01", "02"), 
+            hash("03", "04"),
+            hash("05", "06"),
+            hash("07", "08"),
+            hash("09", "0A"),
+        ]
+
+        root = hash(proof[0], 
+                    hash(
+                        hash(proof[1], 
+                            hash(proof[2], proof[3])
+                        ), 
+                        proof[4]
+                    )
+                )
+
+        self.assertEqual(verify(offsets, proof), root)
 
 if __name__ == '__main__':
     unittest.main()
