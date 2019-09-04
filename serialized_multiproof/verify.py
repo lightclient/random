d94ef0a6e5833d119391e6d28e4503a84893e305
Matt Garnett <14004106+lightclient@users.noreply.github.com>
2019-09-04 13:57:51 -0400

    added larger test case

--- serialized_multiproof/verify.py
+++ serialized_multiproof/verify.py
@@ -3,26 +3,30 @@ import hashlib
 
 
 def verify(offsets, proof, offset=0):
+    # If `offsets` is empty, then we jumped over the end of the current
+    # subtree. The current `offset` will root hash of the right subtree.
     if len(offsets) == 0:
         return proof[offset]
 
     left = proof[offset]
     right = proof[offset + 1]
 
-    # If the first offset is not one, then we have not reached an end node
+    # If the first offset is not 1, then we have not reached an end node
     # so continue to recurse.
     if offsets[0] != 1:
         left = verify(
+            # Pass in the offsets only for the left subtree. 
             offsets[1:(offsets[0])],
             proof,
             offset,
         )
     
     # If there is only 1 offset, then we can be sure that we've reached the
-    # bottom of the tree and the right node can be inferred as left_offset + 1,
-    # Otherwise, recurse on the right side.
+    # bottom of the subtree and the right node can be inferred as
+    # left_offset + 1, Otherwise, recurse on the right side.
     if len(offsets) != 1:
         right = verify(
+            # Pass in the offsets only for the right subtree.
             offsets[offsets[0]:],
             proof,
             offsets[0] + offset,
@@ -40,7 +44,7 @@ def hash(left, right):
 
 def zh(depth):
     if depth == 0:
-        return "0" * 64
+        return "00" * 32
 
     h = zh(depth - 1)
 
@@ -83,6 +87,27 @@ class TestOffsetBuilder(unittest.TestCase):
                 )
 
         self.assertEqual(verify(offsets, proof), root)
+
+    def test_large(self):
+        # indexes = [2, 6, 7168, 7169, 3585, 1793, 897, 449, 225, 113, 57, 29, 15]
+        offsets = [1, 1, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        proof = [
+                zh(11),
+                zh(10),
+                zh(0),
+                zh(0),
+                zh(1),
+                zh(2),
+                zh(3),
+                zh(4),
+                zh(5),
+                zh(6),
+                zh(7),
+                zh(8),
+                zh(9),
+        ]
+
+        self.assertEqual(verify(offsets, proof), zh(12))
 
 if __name__ == '__main__':
     unittest.main()
